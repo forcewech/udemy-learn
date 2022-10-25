ec0555b1ecb8ea13ccb767db9c18a389de37a2dd
Nguyen Tien Dung <forcewech@gmail.com>
2022-10-26 05:19:28 +0700

    video19

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -7,11 +7,20 @@ class MyComponent extends React.Component {
         address: 'Hoi Dan IT',
         age: 20,
     };
+    handleClick(event) {
+        console.log(">> click me my button")
+        console.log(event)
+    }
+    handleOnMoverOver(event) {
+        console.log(event.pageX)
+    }
     //JSX
     render() {
         return (
             <div>
                 My name is {this.state.name} and I'm from {this.state.address}
+                <button onClick={this.handleClick}>Click me</button>
+                <button onMouseOver={this.handleOnMoverOver}>Hover me</button>
             </div>
         );
     }
