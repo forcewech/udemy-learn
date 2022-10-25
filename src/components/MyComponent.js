e51d65ab5e654bf575929c5a21a2f8eaa43eac18
Nguyen Tien Dung <forcewech@gmail.com>
2022-10-25 11:13:29 +0700

    video 17

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -1,10 +1,18 @@
 import React from "react";
 
 class MyComponent extends React.Component {
+
+    state = {
+        name: 'HaryPHamDev',
+        address: 'Hoi Dan IT',
+        age: 20,
+    };
     //JSX
     render() {
         return (
-            <div>my first Component</div>
+            <div>
+                My name is {this.state.name} and I'm from {this.state.address}
+            </div>
         );
     }
 }
